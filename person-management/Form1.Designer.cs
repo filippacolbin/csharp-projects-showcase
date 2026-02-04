d23985c25dd89d96dd062808148ec32d3950a9ff
filippacolbin <filippa.colbin@hotmail.se>
2026-02-04 19:51:38 +0100

    Add files via upload

---
+++ person-management/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿
+namespace PersonApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFornamn = new System.Windows.Forms.TextBox();
+            this.txtEfternamn = new System.Windows.Forms.TextBox();
+            this.txtPersonnummer = new System.Windows.Forms.TextBox();
+            this.txtResultat = new System.Windows.Forms.TextBox();
+            this.btnKontrollera = new System.Windows.Forms.Button();
+            this.btnRensa = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.filToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.registeraEnPersonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.avslutaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtFornamn
+            // 
+            this.txtFornamn.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtFornamn.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFornamn.Location = new System.Drawing.Point(153, 71);
+            this.txtFornamn.Name = "txtFornamn";
+            this.txtFornamn.Size = new System.Drawing.Size(290, 22);
+            this.txtFornamn.TabIndex = 0;
+            // 
+            // txtEfternamn
+            // 
+            this.txtEfternamn.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtEfternamn.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEfternamn.Location = new System.Drawing.Point(153, 111);
+            this.txtEfternamn.Name = "txtEfternamn";
+            this.txtEfternamn.Size = new System.Drawing.Size(290, 22);
+            this.txtEfternamn.TabIndex = 1;
+            // 
+            // txtPersonnummer
+            // 
+            this.txtPersonnummer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPersonnummer.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPersonnummer.Location = new System.Drawing.Point(153, 152);
+            this.txtPersonnummer.Name = "txtPersonnummer";
+            this.txtPersonnummer.Size = new System.Drawing.Size(290, 22);
+            this.txtPersonnummer.TabIndex = 2;
+            // 
+            // txtResultat
+            // 
+            this.txtResultat.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtResultat.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultat.Location = new System.Drawing.Point(153, 207);
+            this.txtResultat.Multiline = true;
+            this.txtResultat.Name = "txtResultat";
+            this.txtResultat.Size = new System.Drawing.Size(290, 75);
+            this.txtResultat.TabIndex = 3;
+            // 
+            // btnKontrollera
+            // 
+            this.btnKontrollera.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKontrollera.Location = new System.Drawing.Point(249, 178);
+            this.btnKontrollera.Name = "btnKontrollera";
+            this.btnKontrollera.Size = new System.Drawing.Size(87, 23);
+            this.btnKontrollera.TabIndex = 4;
+            this.btnKontrollera.Text = "Kontrollera";
+            this.btnKontrollera.UseVisualStyleBackColor = true;
+            this.btnKontrollera.Click += new System.EventHandler(this.btnKontrollera_Click);
+            // 
+            // btnRensa
+            // 
+            this.btnRensa.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRensa.Location = new System.Drawing.Point(249, 288);
+            this.btnRensa.Name = "btnRensa";
+            this.btnRensa.Size = new System.Drawing.Size(87, 23);
+            this.btnRensa.TabIndex = 5;
+            this.btnRensa.Text = "Rensa";
+            this.btnRensa.UseVisualStyleBackColor = true;
+            this.btnRensa.Click += new System.EventHandler(this.btnRensa_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(91, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 15);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Förnamn";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(84, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Efternamn";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(57, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Personnummer";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(141, 43);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(303, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Vänligen fyll i förnamn, efternamn och personnummer";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.filToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(597, 24);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // filToolStripMenuItem
+            // 
+            this.filToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.registeraEnPersonToolStripMenuItem,
+            this.avslutaToolStripMenuItem});
+            this.filToolStripMenuItem.Name = "filToolStripMenuItem";
+            this.filToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.filToolStripMenuItem.Text = "Meny";
+            this.filToolStripMenuItem.Click += new System.EventHandler(this.MenyToolStripMenuItem_Click);
+            // 
+            // registeraEnPersonToolStripMenuItem
+            // 
+            this.registeraEnPersonToolStripMenuItem.Name = "registeraEnPersonToolStripMenuItem";
+            this.registeraEnPersonToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.registeraEnPersonToolStripMenuItem.Text = "Registera en person";
+            this.registeraEnPersonToolStripMenuItem.Click += new System.EventHandler(this.registeraEnPersonToolStripMenuItem_Click);
+            // 
+            // avslutaToolStripMenuItem
+            // 
+            this.avslutaToolStripMenuItem.Name = "avslutaToolStripMenuItem";
+            this.avslutaToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.avslutaToolStripMenuItem.Text = "Avsluta";
+            this.avslutaToolStripMenuItem.Click += new System.EventHandler(this.avslutaToolStripMenuItem_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(212, 116);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(152, 33);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Välkommen!";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(102, 173);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(373, 26);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Klicka på meny för att registrera en person";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(597, 355);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRensa);
+            this.Controls.Add(this.btnKontrollera);
+            this.Controls.Add(this.txtResultat);
+            this.Controls.Add(this.txtPersonnummer);
+            this.Controls.Add(this.txtEfternamn);
+            this.Controls.Add(this.txtFornamn);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Personapp";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFornamn;
+        private System.Windows.Forms.TextBox txtEfternamn;
+        private System.Windows.Forms.TextBox txtPersonnummer;
+        private System.Windows.Forms.TextBox txtResultat;
+        private System.Windows.Forms.Button btnKontrollera;
+        private System.Windows.Forms.Button btnRensa;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem filToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem registeraEnPersonToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem avslutaToolStripMenuItem;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
